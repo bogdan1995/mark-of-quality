05a5f5de012d214c7887e44d23b95576939e65bb
Bogdan Ponomarenko <ponomarenko.bogdan@yandex.ru>
2015-01-29 18:42:54 +0300

    add header and slider

--- proj/js/main.js
+++ proj/js/main.js
@@ -1,5 +1,31 @@
 "use strict";
 
-(function () {
-	console.log('Hello, World!');
+var app =(function () {
+	return {
+		init: function () {
+			app.setUpListeners();
+		},
+		setUpListeners: function () {
+			$('.slider__list').bxSlider(app.SLIDER_OPT);
+			$('.pager-link').text('');
+
+		},
+		SLIDER_OPT: {
+			mode: 'horizontal',
+			infiniteLoop: true,
+			useCSS: false,
+			auto: false,
+			responsive: true,
+			touchEnabled: true,
+			onSlideNext: function () {
+				alert('114');
+			},
+			pager: true
+		},
+		animatePeople: function () {
+
+		}
+	}
 })();
+
+app.init()
